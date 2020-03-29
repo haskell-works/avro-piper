217658e7df29a5ce97317fce7cdaac7dfa44f321
alexeyraga <alexey.raga@gmail.com>
2020-03-30 09:36:54 +1100

    Use Schema

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,28 @@
 module Main where
 
-import           Conduit                     as C
-import           Control.Arrow               (left)
+import           Conduit               as C
+import           Control.Arrow         (left)
 import           Control.Monad
 import           Control.Monad.Except
-import qualified Data.Avro                   as A
-import qualified Data.Avro.Decode            as A
-import qualified Data.Avro.Schema            as A
-import qualified Data.Avro.Types             as A
-import qualified Data.ByteString             as StrictBS
-import qualified Data.ByteString.Char8       as StrictC8
+import qualified Data.Avro             as A
+import qualified Data.Avro.Decode      as A
+import qualified Data.Avro.Schema      as A
+import qualified Data.Avro.Types       as A
+import qualified Data.ByteString       as StrictBS
+import qualified Data.ByteString.Char8 as StrictC8
 
 import           Data.ByteString.Builder     as BB
-import           Data.ByteString.Lazy        (ByteString, fromStrict, null,
-                                              toStrict)
+import           Data.ByteString.Lazy        (ByteString, fromStrict, null, toStrict)
 import qualified Data.ByteString.Lazy        as BS
 import           Data.ByteString.Lazy.Search (split)
 import           Data.Monoid
 import           Kafka.Avro
 
-import           Conduit.Splitter
-import           Data.Aeson                  as J
+import Conduit.Splitter
+import Data.Aeson       as J
 
-import           Error
-import           Options
+import Error
+import Options
 
 
 main :: IO ()
@@ -49,7 +48,7 @@ main = do
     .| stdoutC
 
 
-decodeMessage :: MonadIO m => SchemaRegistry -> ByteString -> m (Either DecodeError (A.Value A.Type))
+decodeMessage :: MonadIO m => SchemaRegistry -> ByteString -> m (Either DecodeError (A.Value A.Schema))
 decodeMessage sr bs = runExceptT $ do
   (sid, payload) <- asExceptTPure id . maybeToEither BadPayloadNoSchemaId $ extractSchemaId bs
   sch            <- asExceptT DecodeRegistryError (loadSchema sr sid)
